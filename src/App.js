fc81751ba0681855f83c569c527f24b5e1d73b7d
jose arjona <josedarjona@gmail.com>
2018-08-04 17:17:54 -0400

    app changes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,105 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import AddTask from './addTask'
+import AddTask from './addTask';
+import EditTask from './editTask'
+
+
 
 class App extends Component {
-
   constructor(props){
     super(props)
+    // this.props.showing = false;
     this.state = {
       theTasks: [],
+      showing: false,
     }
   }
 
+
   getAllTheTasks(){
     axios.get("http://localhost:5000/api/tasks")
-      .then((allTheTasks) => {
-        this.setState({theTasks: allTheTasks.data})
-        // console.log(allTheTasks.data)
-
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    .then((allTheTasks)=>{
+      // console.log(allTheTasks)
+      this.setState({theTasks: allTheTasks.data, showing: false})
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
   }
 
   showTasks(){
-    return(
-      this.state.theTasks.reverse().map(function(task, index){
+    if(this.state.theTasks.length === 0){
+      this.getAllTheTasks();
+    }
+
+    return (
+      this.state.theTasks.map((task, index) =>{
         return(
-          <div key={index}>
-            <h3>{task.title}</h3>
-            <p>{task.description}</p>
-          </div>
-        )
+      <div key={index}>
+      <button onClick={()=> {this.toggleEditForm(index)}} style={{float: 'right', backgroundColor: 'greenyellow', padding: '10px'}}> edit this task </button>
+      <h3>{task.title}</h3>
+      <p >{task.description} </p>
+      {this.renderForm(index, task._id, task.title, task.description)}
+      </div>
+        ) 
       })
     )
   }
 
+  toggleEditForm(whichTask){
 
+    if(this.state.showing === whichTask){
+      this.setState({theTasks: this.state.theTasks, showing: false})
+    }else {
+      this.setState({theTasks: this.state.theTasks, showing: whichTask})
+
+    }
+
+    
+
+    console.log(whichTask)
+
+  }
+
+  renderForm(theIndex, theTaskID, theTitle, theDesc){
+
+
+
+    if(this.state.showing === theIndex){
+    return(
+
+      <EditTask taskProp={theTaskID} title={theTitle} desc={theDesc} blah={()=>this.getAllTheTasks()}></EditTask>
+
+
+
+    )
+  }
+  }
 
 
   render() {
     return (
-      <div className="App">
-        <h1>a to do list for us to show something</h1>
-          <div className="add">
-          <AddTask></AddTask>
-          </div>
-        <div className="list">
-        
-        <button onClick={()=>{this.getAllTheTasks()}}>
-          Get Tasks
-        </button>
-        {this.showTasks()}
-        </div>
+    <div className="App">
+    <h1> The Single Greatest To-Do List In The History of Human History</h1>
+    
+    <div className="add">
+    <AddTask blah={()=>this.getAllTheTasks()}></AddTask>
+    </div>
+    <div className="list">
+      <h2> List of Tasks </h2>
+    {this.showTasks()}
+    </div>
+
       </div>
     );
   }
 }
+
+
+
+
 
 
 
